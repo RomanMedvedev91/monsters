bce6a80fd47413f98164b5c5d608be49c3ce912d
Roman Medvedev <roman.medvedev77@gmail.com>
2022-03-15 18:45:23 -0400

    add title

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "./App.css";
 
 const App = () => {
   const [searchField, setSearchField] = useState("");
+  const [title, setTitle] = useState("Monsters");
   const [monsters, setMonsters] = useState([]);
 
   useEffect(() => {
@@ -17,6 +18,10 @@ const App = () => {
     const searchFieldString = event.target.value.toLocaleLowerCase();
     setSearchField(searchFieldString);
   };
+  const onTitleChange = (event) => {
+    const searchFieldString = event.target.value.toLocaleLowerCase();
+    setTitle(searchFieldString);
+  };
 
   const filteredMonsters = monsters.filter((monster) =>
     monster.name.toLowerCase().includes(searchField.toLowerCase())
@@ -24,11 +29,17 @@ const App = () => {
 
   return (
     <div className='App'>
-      <h1 className='app-title'>Moster Roldex</h1>
+      <h1 className='app-title'>{title}</h1>
       <SearchBox
         className='monsters-search-box'
         placeholder='search monster'
         handleChange={onSearchChange}
+      />
+      <br />
+      <SearchBox
+        className='title-search-box'
+        placeholder='Title'
+        handleChange={onTitleChange}
       />
       <CartList monsters={filteredMonsters} />
     </div>
