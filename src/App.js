bcc721dd8df5bac495bcf553c870f1d99a920594
Roman Medvedev <roman.medvedev77@gmail.com>
2022-03-16 10:50:46 -0400

    clean up app.js

--- src/App.js
+++ src/App.js
@@ -45,42 +45,5 @@ const App = () => {
     </div>
   );
 };
-// class App extends Component {
-//   constructor() {
-//     super();
-
-//     this.state = {
-//       monsters: [],
-//       searchField: "",
-//     };
-//   }
-
-//   componentDidMount() {
-//     fetch("https://jsonplaceholder.typicode.com/users")
-//       .then((res) => res.json())
-//       .then((users) => this.setState({ monsters: users }));
-//   }
-
-//   handleChange = (e) => {
-//     this.setState({ searchField: e.target.value });
-//   };
-
-//   render() {
-//     const { monsters, searchField } = this.state;
-//     const filteredMonsters = monsters.filter((monster) =>
-//       monster.name.toLowerCase().includes(searchField.toLowerCase())
-//     );
-//     return (
-//       <div className='App'>
-//         <h1>Moster Roldex</h1>
-//         <SearchBox
-//           placeholder='search monster'
-//           handleChange={this.handleChange}
-//         />
-//         <CartList monsters={filteredMonsters} />
-//       </div>
-//     );
-//   }
-// }
 
 export default App;
